96763bee4ab3017b1aad2779e253ff98bfd3daea
츄차원의세계 <lazpberry1012@gmail.com>
2023-03-15 22:52:01 +0900

    Document Object and Events

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,25 +1,7 @@
-const calculator = {
-    add: function(a, b)
-    {
-        console.log(a + b);
-    },
-    minus: function(a, b)
-    {
-        console.log(a - b);
-    },
-    divide: function(a, b)
-    {
-        console.log(a / b);
-    },
-    power: function(a, b)
-    {
-        console.log(a ** b);
-    }
+const title = document.querySelector(".hello h1");
+function changeTextColor(clr){
+    title.style.color = clr;
+    console.log("Click!");
 }
-
-const a = parseInt(prompt("Insert a:"));
-const b = parseInt(prompt("Insert b:"));
-calculator.add(a, b);
-calculator.minus(a, b);
-calculator.divide(a, b);
-calculator.power(a, b);
+title.addEventListener("mouseover", () => {changeTextColor("brown")});
+title.addEventListener("mouseout", () => {changeTextColor("black")});
