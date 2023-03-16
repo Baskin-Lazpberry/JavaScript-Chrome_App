6e7d9b61954a64c79687f63820dea328bcd29b67
츄차원의세계 <lazpberry1012@gmail.com>
2023-03-16 22:45:26 +0900

    Main app - Login features

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,6 +1,38 @@
-const h1 = document.querySelector(".hello h1");
-function changeTextColor(){
-    h1.classList.toggle("active");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.getElementById("greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+//Show username with greetings
+function showGreeting(name) {
+    greeting.innerText = `Hello, ${name}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-h1.addEventListener("click", changeTextColor);
+//When login infos are submitted,
+function onLoginSubmit(event) {
+    event.preventDefault();
+
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    showGreeting(username);
+}
+
+//===========================================//
+//                  Main part                //
+//===========================================//
+
+//get username from localStorage
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+
+if(savedUsername === null){ //If there's no saved name
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit)
+} else{ //If there's a saved one.
+    showGreeting(savedUsername);
+}
