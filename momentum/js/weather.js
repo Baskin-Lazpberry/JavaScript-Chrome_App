c7332290793886d6f56a065f4bc57d84599ff748
츄차원의세계 <lazpberry1012@gmail.com>
2023-03-19 23:55:17 +0900

    Implemented Weather API

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,5 +1,13 @@
+
+const API_KEY = "";
+
 function onGeoOk(position){
-    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?
+    lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    console.log(url);
+    fetch(url);
 }
 
 function onGeoError(){
