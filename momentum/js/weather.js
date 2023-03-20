552a4f8a4dc5a5acb845ac3c2c6cb0df35a1c1e7
츄차원의세계 <lazpberry1012@gmail.com>
2023-03-20 23:40:40 +0900

    Add city, weather indicator

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,13 +1,19 @@
 
-const API_KEY = "";
+const API_KEY = "f9bdf6724e300a1b2e7a621c76d17876";
 
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?
-    lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
     console.log(url);
-    fetch(url);
+    fetch(url).then(response => response.json()
+    .then((data) => {
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+
+        city.innerText  = data.name + ",";
+        weather.innerText = data.weather[0].main;
+    }));
 }
 
 function onGeoError(){
